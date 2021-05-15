35677cad7fd893b31507d604bb74d4683787c161
TravisEvans <travisjamesevans01@gmail.com>
2021-05-15 15:22:23 +1000

    attempting sql v2

--- functions.py
+++ functions.py
@@ -4,15 +4,34 @@ from discord.ext import commands
 import os
 import psycopg2
 
-async def populate_table():
+async def make_table():
     DATABASE_URL = os.environ['DATABASE_URL']
 
+    # Connect to an existing database
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-    cur.execute("""
-        INSERT INTO some_table (an_int, a_date, a_string)
-        VALUES (%s, %s, %s);
-        """, (10, datetime.date(2005, 11, 18), "O'Reilly"))
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
+
+    # Execute a command: this creates a new table
+    cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+
+    # Pass data to fill a query placeholders and let Psycopg perform
+    # the correct conversion (no more SQL injections!)
+    cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)",
+                    ...      (100, "abc'def"))
+        
+    # Query the database and obtain data as Python objects
+    cur.execute("SELECT * FROM test;")
+    cur.fetchone()
+    (1, 100, "abc'def")
+
+    # Make the changes to the database persistent
+    conn.commit()
+    
+    # Close communication with the database
+    cur.close()
+    conn.close()
 
 
 async def reminder(client, message_called_from):
