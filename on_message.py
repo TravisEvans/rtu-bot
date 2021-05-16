3e57438bf56ca2f938a36824a50ad018117c6691
TravisEvans <travisjamesevans01@gmail.com>
2021-05-16 21:19:55 +1000

    I hate the current issues

--- on_message.py
+++ on_message.py
@@ -13,54 +13,54 @@ async def receive_message(client, message):
     if message.author == client.user:  # check if it is me( bot ) messaging
         return
 
-    if str(message.author) == "celery#9490":
-        if message.content == "state":
+    if str(message.author) == "celery#9490" or str(message.author) == "Soviet Attack Puss#0033":
+        if message.content == "_state":
             await message.channel.send("functional")
 
         #   reminder function   #   TEMPORARY, NEED TO STICK THIS INTO AN AUTOMATED THING
-        if message.content == "reminder":
+        if message.content == "_reminder":
             print("running reminder")
             await functions.reminder(client, message)
             print("reminder finished\n")
             finMessage = "__Finished__: " + message.content + " function"
             await message.channel.send(finMessage)
 
-        if message.content == "make_table":
+        if message.content == "_make_table":
             print("running make_table")
             await functions.make_table(client, message)
             print("make_table finished\n")
             finMessage = "__Finished__: " + message.content + " function"
             await message.channel.send(finMessage)
 
-        if message.content == "view_table":
+        if message.content == "_view_table":
             print("running view_table")
             await functions.view_table(client, message)
             print("view_table finished\n")
             finMessage = "__Finished__: " + message.content + " function"
             await message.channel.send(finMessage)
 
-        if message.content == "add_to_table":
+        if message.content == "_add_to_table":
             print("running add_to_table")
             await functions.add_to_table(client, message)
             print("add_to_table finished\n")
             finMessage = "__Finished__: " + message.content + " function"
             await message.channel.send(finMessage)
 
-        if message.content == "clear_table":
+        if message.content == "_clear_table":
             print("running clear_table")
             await functions.clear_table(client, message)
             print("clear_table finished\n")
             finMessage = "__Finished__: " + message.content + " function"
             await message.channel.send(finMessage)
 
-        if message.content == "delete_table":
+        if message.content == "_delete_table":
             print("running delete_table")
             await functions.delete_table(client, message)
             print("delete_table finished\n")
             finMessage = "__Finished__: " + message.content + " function"
             await message.channel.send(finMessage)
         
-        if message.content == "help":
+        if message.content == "_help":
             print("running help")
             await functions.help(client, message)
             print("help finished\n")
@@ -68,7 +68,7 @@ async def receive_message(client, message):
             await message.channel.send(finMessage)
 
 
-        if message.content == "stop":
+        if message.content == "_stop":
             await client.get_channel(message.channel.id).send(
                 """__***BOT SHUTTING DOWN***__""")
             sys.exit()
