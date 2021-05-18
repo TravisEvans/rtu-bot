a3067e0a6ba6cebf230013d4b5fa53f81c6da69c
Travis Evans <travisjamesevans01@gmail.com>
2021-05-18 19:25:41 +1000

    PORT functionality with express is important(?)

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+//  Heroku
+var PORT = process.env.PORT || 5000;
+
+//  Express
+var express = require('express');
+var app = express();
+var http = require('http');
+var server = http.Server(app);
+
+app.use(express.static('client'))
+
+server.listen(PORT, () =>   {
+    console.log("Bot launched");
+})
+
+///////////////////////////////////////////////dont know what these are
+// var io = require('socket.io')(server);
+
+// io.on('connection', (socket) => {
+//     socket.on('message', (msg) => {
+//         io.emit('message', msg);
+//     })
+// })
+///////////////////////////////////////////////dont know what these are
+        
+//  Discord.js
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
