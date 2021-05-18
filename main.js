e721fbc8b5a0df6b40c9ec00982bde39ade24399
Travis Evans <travisjamesevans01@gmail.com>
2021-05-18 19:28:36 +1000

    PORT functionality with express is important(?)

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ server.listen(PORT, () =>   {
 // var io = require('socket.io')(server);
 
 // io.on('connection', (socket) => {
-//     socket.on('message', (msg) => {
+    //     socket.on('message', (msg) => {
 //         io.emit('message', msg);
 //     })
 // })
